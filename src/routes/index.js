d4b2e98f4604e24a2bb013ba38943138cebc8c7f
Farizal Tri Anugrah <omdjin@gmail.com>
2019-01-13 14:33:59 +0700

    Clean up and add router

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { object } from "prop-types";
+import { Route, Switch } from "react-router-dom";
+
+import HomeComponent from "./Home";
+import ProductContainer from "./Product";
+
+const Routes = ({ history, location, match }) => (
+  <div>
+    <segment>Header</segment>
+    <Switch>
+      <Route path="/:domain" component={ProductContainer} />
+      <Route exact path="/" component={HomeComponent} />
+    </Switch>
+    <segment>Footer</segment>
+  </div>
+);
+
+Routes.propTypes = {
+  history: object.isRequired,
+  location: object.isRequired,
+  match: object.isRequired
+};
+
+const RootRoutes = () => <Route component={Routes} />;
+
+export default RootRoutes;
