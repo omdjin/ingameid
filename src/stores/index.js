edb25b451888799902b4e0c5603fdf4c75b28079
omdjin <omdjin@gmail.com>
2019-02-03 22:31:51 +0700

    Add GPID and add redirect product

--- src/stores/index.js
+++ src/stores/index.js
@@ -5,31 +5,42 @@ export const products = [
     body:
       '<p>Beli PUBG-Mobile Unknown Cash (UC) murah dan mudah dengan berbagai nominal!</p><p>Pilih nominal UC yang kamu inginkan dan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.</p><ul><li>PUBG UC-50 = Rp11.000</li><li>PUBG UC-70 Rp14.600</li><li>PUBG UC-150 = Rp31.100</li><li>PUBG UC-210 = Rp43.900</li><li>PUBG UC-250 = Rp52.000</li><li>PUBG UC-500 = Rp103.000</li><li>PUBG UC-700 = Rp146.300</li><li>PUBG UC-1250 = Rp258.500</li><li>PUBG UC-1750 = Rp364.600</li><li>PUBG UC-2500 = Rp519.700</li><li>PUBG UC-3500 = Rp729.300</li></ul>',
     shortDesc:
-      'Beli Beli PUBG-Mobile Unknown Cash (UC) murah dan mudah dengan berbagai nominal hanya di Ingame.id! Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.',
+      'Beli PUBG-Mobile Unknown Cash (UC) murah dan mudah dengan berbagai nominal hanya di Ingame.id! Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.',
     imageUrl: '/static/img/pubg-mobile.webp',
     sku: 'PUBG',
     brand: 'Tencent Games',
   },
   {
-    slug: 'mobile-legend-starlight',
-    title: 'Mobile Legends Starlight',
+    slug: 'google-play-giftcard',
+    title: 'Google Play Gift Card Indonesia',
     body:
-      '<p>Beli Starlight Member Mobile Legends murah dan mudah dengan berbagai nominal!</p><p>Pilih nominal yang kamu inginkan dan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.</p><ul><li>ML Starlight 30D = Rp149.000</li><li>ML Seasonal Starlight 93D + 448 D = Rp480.000</li><li>Starlight + 1230 Diamond = Rp485.000</li></ul>',
+      '<p>Ready stock Google Play Gift Card Indonesia murah dan mudah dengan berbagai nominal! Satu tempat untuk segala bentuk hiburan: Film, buku, majalah, atau aplikasi dan game Android terbaru.</p><p>Pilih nominal UC yang kamu inginkan dan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.</p><ul><li>ID 20.000=Rp 24.000</li><li>ID 50.000=Rp 58.000</li><li>ID 100.000=Rp 115.000</li><li>ID 150.000=Rp 170.000</li><li>ID 300.000=Rp 335.000</li><li>ID 500.000=Rp 555.000</li></ul><p>Cara memasukkan kode di web:</p><ul><li>Buka situs <a href="https://play.google.com/redeem">play.google.com/redeem</a></li><li>Masukkan kode Google Play</li><li>Mulailah berbelanja</li></ul><p>Cara memasukkan kode di Android phone/tablet:</p><ul><li>Buka aplikasi Google Play Store di Android phone atau tablet kamu</li><li>Pilih tukarkan</li><li>Masukkan kode Google Play</li><li>Mulailah berbelanja</li></ul>',
     shortDesc:
-      'Beli Starlight Member Mobile Legends murah dan mudah dengan berbagai nominal hanya di Ingame.id! Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.',
-    imageUrl: '/static/img/mobile-legend-starlight.webp',
-    sku: 'ML-STR',
-    brand: 'Moonton',
+      'Beli Google Play Gift Card Indonesia murah dan mudah dengan berbagai nominal! Satu tempat untuk segala bentuk hiburan: Film, buku, majalah, atau aplikasi dan game Android terbaru.',
+    imageUrl: '/static/img/google-play-giftcard.webp',
+    sku: 'GPID',
+    brand: 'Google',
   },
   {
-    slug: 'mobile-legend-diamond',
-    title: 'Mobile Legends Diamond',
+    slug: 'ragnarok-m-eternal-love-zeny',
+    title: 'Ragnarok M: Eternal Love Zeny',
     body:
-      '<p>Beli Diamond Mobile Legends murah dan mudah dengan berbagai nominal hanya di Ingame.id!</p><p>Pilih nominal yang kamu inginkan dan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.</p><p>36 Diamond = Rp10.000<br />74 Diamond = Rp20.000<br />220 Diamond = Rp60.000<br />275 Diamond = Rp75.000<br />366 Diamond = Rp100.000<br />966 diamond = Rp250.000<br />2010 Diamond = Rp495.000<br />4804 Diamond = Rp1.200.000</p>',
+      '<p>Ready stock Ragnarok M: Eternal Love Zeny dengan berbagai nominal. Top Up menggunakan user ID tanpa perlu login..!</p><p>Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.</p><p>Ragnarok M: Eternal Love adalah sebuah game mobile petualangan MMORPG yang didasari dari populernya Ragnarok Online di tahun 2003, dimana game ini merupakan bagian warisan dari jutaan pecinta RO di seluruh dunia. Ragnarok M: Eternal Love memamerkan latar belakang kemegahan Rune Midgard dengan tetap mempertahankan seni artistiknya selama 15 tahun. Fitur gameplay klasik yang dilengkapi dengan beberapa sentuhan modern!</p><ul><li>40rb Zeny=Rp 10.000</li><li>54rb Zeny=Rp 15,000</li><li>80rb Zeny=Rp 20.000</li><li>120rb Zeny=Rp 30.000</li><li>200rb Zeny=Rp 50.000</li><li>300rb Zeny=Rp 75.000</li><li>400rb Zeny=Rp 100.000</li><li>600rb Zeny=Rp 148.000</li><li>800rb Zeny=Rp 195.000</li><li>1,2jt Zeny=Rp 297.000</li><li>2jt Zeny=Rp 490.000</li><li>2,98jt Zeny=Rp 730.000</li><li>4jt Zeny=Rp 980.000</li></ul>',
     shortDesc:
-      'Beli Diamond Mobile Legends murah dan mudah dengan berbagai nominal hanya di Ingame.id! Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.',
-    imageUrl: '/static/img/mobile-legend-diamond.webp',
-    sku: 'ML-DMN',
+      'Ready stock Ragnarok M: Eternal Love Zeny dengan berbagai nominal. Top Up menggunakan user ID tanpa perlu login..! Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.',
+    imageUrl: '/static/img/ragnarok-m-zeny.webp',
+    sku: 'RGZ',
+    brand: 'Lytogame',
+  },
+  {
+    slug: 'diamond-starlight-member-mobile-legend',
+    title: 'Diamond & Startlight Member Mobile Legends',
+    body:
+      '<p>Topup Diamond atau Startlight Member Mobile Legends murah dan mudah dengan berbagai nominal hanya di Ingame.id! Cukup dengan mengirimkan screenshoot User ID Mobile Legend kamu.</p><p>Pilih nominal yang kamu inginkan dan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.</p><ul><li>36 Diamond=Rp10.000</li><li>74 Diamond=Rp20.000</li><li>220 Diamond=Rp60.000</li><li>275 Diamond=Rp75.000</li><li>366 Diamond=Rp100.000</li><li>966 diamond=Rp250.000</li><li>2010 Diamond=Rp495.000</li><li>4804 Diamond=Rp1.200.000</li></ul><ul><li>ML Starlight 30 Hari=Rp 150.000</li><li>ML Starlight 30 Hari + 127 Dm=Rp 200.000</li><li>ML Starlight 30 Hari + 390 Dm=Rp 250.000</li><li>ML Starlight 30 Hari + 1230 Dm=Rp 500.000</li><li>Seasonal Starlight 93 Hari + 448 Dm=Rp 1.000.000</li></ul>',
+    shortDesc:
+      'Topup Diamond atau Startlight Member Mobile Legends murah dan mudah dengan berbagai nominal hanya di Ingame.id! Cukup dengan mengirimkan screenshoot User ID Mobile Legend kamu. Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.',
+    imageUrl: '/static/img/mobile-legend.webp',
+    sku: 'ML',
     brand: 'Moonton',
   },
   {
@@ -42,17 +53,6 @@ export const products = [
     imageUrl: '/static/img/steam-wallet-idr.webp',
     sku: 'SWID',
     brand: 'Steam',
-  },
-  {
-    slug: 'ragnarok-m-eternal-love-zeny',
-    title: 'Ragnarok M: Eternal Love Zeny',
-    body:
-      '<p>Ready stock Ragnarok M: Eternal Love Zeny dengan berbagai nominal. Top Up menggunakan user ID tanpa perlu login..!</p><p>Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.</p><p>Ragnarok M: Eternal Love adalah sebuah game mobile petualangan MMORPG yang didasari dari populernya Ragnarok Online di tahun 2003, dimana game ini merupakan bagian warisan dari jutaan pecinta RO di seluruh dunia. Ragnarok M: Eternal Love memamerkan latar belakang kemegahan Rune Midgard dengan tetap mempertahankan seni artistiknya selama 15 tahun. Fitur gameplay klasik yang dilengkapi dengan beberapa sentuhan modern!</p><ul><li>40rb Zeny=Rp 10.000</li><li>54rb Zeny=Rp 15,000</li><li>80rb Zeny=Rp 20.000</li><li>120rb Zeny=Rp 30.000</li><li>200rb Zeny=Rp 50.000</li><li>300rb Zeny=Rp 75.000</li><li>400rb Zeny=Rp 100.000</li><li>600rb Zeny=Rp 148.000</li><li>800rb Zeny=Rp 195.000</li><li>1,2jt Zeny=Rp 297.000</li><li>2jt Zeny=Rp 490.000</li><li>2,98jt Zeny=Rp 730.000</li><li>4jt Zeny=Rp 980.000</li></ul>',
-    shortDesc:
-      'Ready stock Ragnarok M: Eternal Love Zeny dengan berbagai nominal. Top Up menggunakan user ID tanpa perlu login..! Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.',
-    imageUrl: '/static/img/ragnarok-m-zeny.webp',
-    sku: 'RGZ',
-    brand: 'Lytogame',
   },
 ];
 
