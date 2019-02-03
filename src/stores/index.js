957846d2546afe06af689a55fe311280838ce547
omdjin <omdjin@gmail.com>
2019-02-03 21:54:46 +0700

    Add pubgand move image to static

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,16 +1,12 @@
-import steamWallet from './img/steam-wallet-idr.webp';
-import mlStarlight from './img/mobile-legend-starlight.webp';
-import mlDiamond from './img/mobile-legend-diamond.webp';
-import pubgWebp from './img/pubg-mobile.webp';
-
 export const products = [
   {
     slug: 'pubgm-unknown-cash',
     title: 'PUBG-Mobile Unknown Cash (UC)',
     body:
       '<p>Beli PUBG-Mobile Unknown Cash (UC) murah dan mudah dengan berbagai nominal!</p><p>Pilih nominal UC yang kamu inginkan dan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.</p><ul><li>PUBG UC-50 = Rp11.000</li><li>PUBG UC-70 Rp14.600</li><li>PUBG UC-150 = Rp31.100</li><li>PUBG UC-210 = Rp43.900</li><li>PUBG UC-250 = Rp52.000</li><li>PUBG UC-500 = Rp103.000</li><li>PUBG UC-700 = Rp146.300</li><li>PUBG UC-1250 = Rp258.500</li><li>PUBG UC-1750 = Rp364.600</li><li>PUBG UC-2500 = Rp519.700</li><li>PUBG UC-3500 = Rp729.300</li></ul>',
-    shortDesc: 'Beli Beli PUBG-Mobile Unknown Cash (UC) murah dan mudah dengan berbagai nominal hanya di Ingame.id!',
-    imageUrl: pubgWebp,
+    shortDesc:
+      'Beli Beli PUBG-Mobile Unknown Cash (UC) murah dan mudah dengan berbagai nominal hanya di Ingame.id! Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.',
+    imageUrl: '/static/img/pubg-mobile.webp',
     sku: 'PUBG',
     brand: 'Tencent Games',
   },
@@ -19,8 +15,9 @@ export const products = [
     title: 'Mobile Legends Starlight',
     body:
       '<p>Beli Starlight Member Mobile Legends murah dan mudah dengan berbagai nominal!</p><p>Pilih nominal yang kamu inginkan dan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.</p><ul><li>ML Starlight 30D = Rp149.000</li><li>ML Seasonal Starlight 93D + 448 D = Rp480.000</li><li>Starlight + 1230 Diamond = Rp485.000</li></ul>',
-    shortDesc: 'Beli Starlight Member Mobile Legends murah dan mudah dengan berbagai nominal hanya di Ingame.id!',
-    imageUrl: mlStarlight,
+    shortDesc:
+      'Beli Starlight Member Mobile Legends murah dan mudah dengan berbagai nominal hanya di Ingame.id! Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.',
+    imageUrl: '/static/img/mobile-legend-starlight.webp',
     sku: 'ML-STR',
     brand: 'Moonton',
   },
@@ -29,8 +26,9 @@ export const products = [
     title: 'Mobile Legends Diamond',
     body:
       '<p>Beli Diamond Mobile Legends murah dan mudah dengan berbagai nominal hanya di Ingame.id!</p><p>Pilih nominal yang kamu inginkan dan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.</p><p>36 Diamond = Rp10.000<br />74 Diamond = Rp20.000<br />220 Diamond = Rp60.000<br />275 Diamond = Rp75.000<br />366 Diamond = Rp100.000<br />966 diamond = Rp250.000<br />2010 Diamond = Rp495.000<br />4804 Diamond = Rp1.200.000</p>',
-    shortDesc: 'Beli Diamond Mobile Legends murah dan mudah dengan berbagai nominal hanya di Ingame.id!',
-    imageUrl: mlDiamond,
+    shortDesc:
+      'Beli Diamond Mobile Legends murah dan mudah dengan berbagai nominal hanya di Ingame.id! Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.',
+    imageUrl: '/static/img/mobile-legend-diamond.webp',
     sku: 'ML-DMN',
     brand: 'Moonton',
   },
@@ -39,10 +37,22 @@ export const products = [
     title: 'Steam Wallet IDR',
     body:
       '<p>Ready stock Steam Wallet ID dengan berbagai nominal!</p><p>Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.</p><p>ID 6.000 =Rp8,000<br />ID 8.000 =Rp10,000<br />ID 12.000 =Rp15,000<br />ID 45.000 =Rp55,000<br />ID 60.000 =Rp72,000<br />ID 90.000 =Rp107,000<br />ID 120.000 =Rp140,000<br />ID 250.000 =Rp275,000<br />ID 400.000 =Rp440,000<br />ID 600.000 =Rp640,000</p>',
-    shortDesc: 'Ready stock Steam Wallet ID dengan berbagai nominal!',
-    imageUrl: steamWallet,
+    shortDesc:
+      'Ready stock Steam Wallet ID dengan berbagai nominal! Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.',
+    imageUrl: '/static/img/steam-wallet-idr.webp',
     sku: 'SWID',
     brand: 'Steam',
+  },
+  {
+    slug: 'ragnarok-m-eternal-love-zeny',
+    title: 'Ragnarok M: Eternal Love Zeny',
+    body:
+      '<p>Ready stock Ragnarok M: Eternal Love Zeny dengan berbagai nominal. Top Up menggunakan user ID tanpa perlu login..!</p><p>Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.</p><p>Ragnarok M: Eternal Love adalah sebuah game mobile petualangan MMORPG yang didasari dari populernya Ragnarok Online di tahun 2003, dimana game ini merupakan bagian warisan dari jutaan pecinta RO di seluruh dunia. Ragnarok M: Eternal Love memamerkan latar belakang kemegahan Rune Midgard dengan tetap mempertahankan seni artistiknya selama 15 tahun. Fitur gameplay klasik yang dilengkapi dengan beberapa sentuhan modern!</p><ul><li>40rb Zeny=Rp 10.000</li><li>54rb Zeny=Rp 15,000</li><li>80rb Zeny=Rp 20.000</li><li>120rb Zeny=Rp 30.000</li><li>200rb Zeny=Rp 50.000</li><li>300rb Zeny=Rp 75.000</li><li>400rb Zeny=Rp 100.000</li><li>600rb Zeny=Rp 148.000</li><li>800rb Zeny=Rp 195.000</li><li>1,2jt Zeny=Rp 297.000</li><li>2jt Zeny=Rp 490.000</li><li>2,98jt Zeny=Rp 730.000</li><li>4jt Zeny=Rp 980.000</li></ul>',
+    shortDesc:
+      'Ready stock Ragnarok M: Eternal Love Zeny dengan berbagai nominal. Top Up menggunakan user ID tanpa perlu login..! Pilih nominal yang kamu inginkan dan bayar melalui BCA, Mandiri, BNI, Jenius(BTPN) dan Indomaret.',
+    imageUrl: '/static/img/ragnarok-m-zeny.webp',
+    sku: 'RGZ',
+    brand: 'Lytogame',
   },
 ];
 
