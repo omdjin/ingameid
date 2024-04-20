4994c863c37ecb12bfb8233d6df95636dfe71ee3
Farizal Tri Anugrah <omdjin@gmail.com>
2024-04-20 17:51:05 +0700

    feat(seo): integrate yoast plugin (#47)

--- pages/tag/[slug].js
+++ pages/tag/[slug].js
@@ -6,23 +6,25 @@ import { heading, container, row } from "styles/tag.css";
 export default function TagPage({ tagDetail, blogs }) {
   const pageTitle = `Tag: ${tagDetail.name} - ${SITE_NAME}`;
   const metaUrl = `${HOSTNAME}/tag/${tagDetail.slug}`;
+  const tagDesc = tagDetail.description;
   const parsedExcerpt = `Baca artikel-artikel di ${SITE_NAME} dengan tag ${tagDetail.name}.`;
+  const metaDesc = tagDesc || parsedExcerpt;
 
   return (
     <>
       <Head>
         <title>{pageTitle}</title>
-        <meta name="description" content={parsedExcerpt} />
+        <meta name="description" content={metaDesc} />
         <link rel="canonical" href={metaUrl} />
         <meta property="og:title" content={pageTitle} />
-        <meta property="og:description" content={parsedExcerpt} />
+        <meta property="og:description" content={metaDesc} />
         <meta property="og:url" content={metaUrl} />
         <meta property="og:site_name" content={SITE_NAME} />
         <meta property="og:locale" content="id_ID" />
         <meta property="og:type" content="article" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={pageTitle} />
-        <meta name="twitter:description" content={parsedExcerpt} />
+        <meta name="twitter:description" content={metaDesc} />
         <meta name="twitter:site" content="@ratriretno" />
         <meta name="twitter:creator" content="@ratriretno" />
       </Head>
